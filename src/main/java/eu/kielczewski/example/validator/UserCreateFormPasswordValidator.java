53f8d1e4574762782df031323d66956c2e8468e0
bkielczewski <bartosz@kielczewski.eu>
2014-04-21 23:17:07 +0100

    Doesn't need to be session-scoped, was I drunk?

--- src/main/java/eu/kielczewski/example/validator/UserCreateFormPasswordValidator.java
+++ src/main/java/eu/kielczewski/example/validator/UserCreateFormPasswordValidator.java
@@ -1,24 +1,16 @@
 package eu.kielczewski.example.validator;
 
 import eu.kielczewski.example.domain.UserCreateForm;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
-import org.springframework.web.context.WebApplicationContext;
 
 @Component
-@Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class UserCreateFormPasswordValidator implements Validator {
 
     @Override
     public boolean supports(Class<?> clazz) {
-        if (UserCreateForm.class.isAssignableFrom(clazz)) {
-            return true;
-        } else {
-            return false;
-        }
+        return UserCreateForm.class.isAssignableFrom(clazz);
     }
 
     @Override
